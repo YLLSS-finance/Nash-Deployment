69285743c14e1798cada5add3e5c0ab792f89d49
yllssnospinning <mrnospinning@gmail.com>
2025-12-12 10:11:15 +0800

    committing to github next to future super zhuang yuan lol

--- OPEN DAY/main.py
+++ OPEN DAY/main.py
@@ -11,35 +11,40 @@ from orderProperties import orderProperties as op
 
 class exchange:
     def __init__(self):
+        # userID: [balance, availableBalance]
         self.userBalances = {}
         self.userOrders = {}
-        
+
         # {mpid:{contractID:[pos, red]}}
         self.userPositions = {}
-        
+
         self.contracts = {}
-    
+        self.marginManagers = {}
+
+    def from_state(self, userBalances, userOrders, userPositions):
+        pass
+
     # TODO: Logging
     def _fillOrder(self, userBalances, order, price, qty):
         fillRed = min(qty, order[6][0])
         fillInc = qty - fillRed
-         
+
         priceImprovement = abs(price - order.price)
         orderCost = op.orderCost(order)
-        
+
         userBalances = self.userBalances[mpid]
         saleRevenue = fillRed * (orderCost + priceImprovement)
         increaseCost = fillInc * (orderCost - priceImprovement)
-        
+
         userBalances[1] += priceImprovement * fillInc + saleRevenue
         userBalances[0] += -increaseCost + saleRevenue
-        
+
         if sum(order[6]) == 0:
             self.removeOrder(order, skip_balance=True)
-        
+
     def fillOrder(self, mpid, orderID, price, qty):
-        self._fillOrder(order=self.userOrders[mpid][orderID], price=price, qty=qty)
-    
+        self._fillOrder(userBalances=self.userBalances[mpid], order=self.userOrders[mpid][orderID], price=price, qty=qty)
+
     # TODO: Logging
     def removeOrder(self, order, skip_balance=False):
         mpid = order[2]
@@ -48,4 +53,3 @@ class exchange:
         self.marginManagers[mpid][order[3]].allocReducible()
         if not skip_balance:
             self.userBalances[mpid][1] += op.orderCost(order) * order[6][1]
-            
--- OPEN DAY/marginManager.py
+++ OPEN DAY/marginManager.py
@@ -2,5 +2,13 @@
 class marginManager:
     def __init__(self, _master, mpid, contractID):
         self.userPositions = self._master.userPositions[mpid]
-        if not contractID in self.userPositions:
-        self.userContractPositions = self.userPositions
+        if not contractID in self.userPositions: self.userPositions[contractID] = [[0, 0], [0, 0]]
+        
+        self.position, self.reducible = self.userPositions[contractID]
+        # This has to be done as we are going to establish the reducible position manually for integrity
+        self.reducible = self.position
+        
+        for order in _master.orders.items():
+            if order[3] == contractID:
+                self.reducible[1 - order[5]] -= order[6][0]
+        
