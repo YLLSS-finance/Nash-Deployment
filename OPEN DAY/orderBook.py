fe6c7f6895ac7796fba28d1a8aac6a72b12f989c
yllssnospinning <mrnospinning@gmail.com>
2025-12-12 14:15:54 +0800

    Fixed reference bug in the order book

--- OPEN DAY/orderBook.py
+++ OPEN DAY/orderBook.py
@@ -16,32 +16,34 @@ class orderBook:
 
     def addOrder(self, order):
         price = order[4]
-        orderDict = self.levels[order[5]]
-        orderHeadTail = order[7]
-        if not price in orderDict.keys():
-            orderDict[price] = [order, order, 0, 0]
-            orderHeadTail = [None, None]
+        order_dict = self.levels[order[5]]
+        order_headtail = order[7]
+        if not price in order_dict.keys():
+            order_dict[price] = [order, order, 0, 0]
+            order_headtail[0] = None
+            order_headtail[1] = None
             return
 
-        priceLevelInfo = orderDict[order.price]
-        priceLevelInfo[1] = order
-        orderHeadTail[0] = priceLevelInfo[1]
-        orderHeadTail[1] = None
+        pricelevel_info = orderDict[order.price]
+        pricelevel_info[1] = order
+        order_headtail[0] = pricelevel_info[1]
+        order_headtail[1] = None
 
-        priceLevelInfo[2] += sum(order[6])
-        priceLevelInfo[3] += 1
+        pricelevel_info[2] += sum(order[6])
+        pricelevel_info[3] += 1
 
     # CAUTION: this assumes that the order is already a member of the book
     def removeOrder(self, order):
-        priceLevelInfo = self.levels[order[5]][order[4]]
-        if order == priceLevelInfo[0]:
-            priceLevelInfo[0] = order[7][1]
-        elif order == priceLevelInfo[1]:
-            priceLevelInfo[1] = order[7][0]
-        priceLevelInfo[2] -= sum(order[6])
-        priceLevelInfo[3] -= 1
+        pricelevel_info = self.levels[order[5]][order[4]]
+        if order == pricelevel_info[0]:
+            pricelevel_info[0] = order[7][1]
+        elif order == pricelevel_info[1]:
+            pricelevel_info[1] = order[7][0]
+        pricelevel_info[2] -= sum(order[6])
+        pricelevel_info[3] -= 1
 
-        orderHead = order[7][0]
-        orderTail = order[7][1]
-        orderHead[7][1] = orderTail
+        order_head = order[7][0]
+        order_tail = order[7][1]
+        order_head[7][1] = orderTail
         orderTail[7][0] = orderHead
+
